150f1be5c4a164cf254c90e1c9876a2c3dccc59e
Joakim Sundman <joakimsundman89@gmail.com>
2024-03-26 20:02:38 +0100

    added modal to gallery

--- client/packages/base/imgGallery/ui/index.tsx
+++ client/packages/base/imgGallery/ui/index.tsx
@@ -1,11 +1,28 @@
 import { useParams } from "@tanstack/react-router";
 import { useGalleryQuery } from "..";
 import "./style.scss";
+import { useState } from "react";
+import { ModalContainer } from "@revyn/modalcontainer";
 
 export const ImageGallery = () => {
   const { year }: { year: string } = useParams({ strict: false });
   const { galleryQuery, galleryImages } = useGalleryQuery(year);
   const imageUrl = import.meta.env.VITE_AWS_IMAGEBUCKET_URL;
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [modalPosition, setModalPosition] = useState({ x: 0, y: 0 });
+
+  const handleImageClick = (
+    image: string,
+    event: React.MouseEvent<HTMLImageElement>
+  ) => {
+    setSelectedImage(image);
+
+    const rect = event.currentTarget.getBoundingClientRect();
+    const x = event.clientX - rect.left + window.scrollX;
+    const y = event.clientY - rect.top + window.scrollY;
+
+    setModalPosition({ x, y });
+  };
 
   return (
     <>
@@ -21,9 +38,26 @@ export const ImageGallery = () => {
               src={`${imageUrl}Galleri/${year}/${image}`}
               alt={`gallery__items--item ${year}`}
               className="gallery__items--item"
+              onClick={(event) => handleImageClick(image, event)}
             />
           ))}
         </section>
+      )}
+      {window.innerWidth > 768 && selectedImage && (
+        <ModalContainer
+          style={{
+            top: modalPosition.y,
+            left: modalPosition.x,
+            translate: "50%",
+          }}
+        >
+          <img
+            src={`${imageUrl}Galleri/${year}/${selectedImage}`}
+            alt={`gallery__modal--image ${year}`}
+            className="gallery__modal--image"
+            onClick={() => setSelectedImage(null)}
+          />
+        </ModalContainer>
       )}
     </>
   );
