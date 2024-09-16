1e9fa0fe000a1c6bab324be59fba90f385cb45e4
Joakim Sundman <joakimsundman89@gmail.com>
2024-09-16 10:48:42 +0200

    removed form from contact page

--- client/packages/pages/contact/ui/index.tsx
+++ client/packages/pages/contact/ui/index.tsx
@@ -1,12 +1,8 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { useRef } from "react";
-import { useSendEmail } from "../data";
-import { FormContent } from "./feature/FormContent";
 import "./style.scss";
 
 export const ContactPage = () => {
   const mapurl = import.meta.env.VITE_MAP_URL;
-  const form = useRef<HTMLFormElement>(null);
   return (
     <>
       <main className="contact container">
@@ -16,16 +12,9 @@ export const ContactPage = () => {
             Har du några frågor eller funderingar? <br />
             Tveka inte att kontakta oss!
           </p>
-          <p>Skicka ett mail &#8594;</p>
+          <a href="mailto:info@ostersundsrevyn.com">Skicka ett mail &#8594;</a>
           <p>Eller ring 070-270 60 05</p>
         </section>
-        <form
-          ref={form}
-          onSubmit={(e) => useSendEmail({ e, form })}
-          className="contact__form"
-        >
-          <FormContent />
-        </form>
         <section className="contact__map">
           <a href={mapurl} target="_blank" rel="noopener noreferrer">
             <img src="./map.png" alt="map" className="contact__map--image" />
